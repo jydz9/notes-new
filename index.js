28a0c8252435a329d9fce8753ccd27d4f3c6bde1
Jim Yang <76235469+jydz9@users.noreply.github.com>
2022-08-15 12:23:43 -0500

    update

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ let notes = [
       content: "GET and POST are the most important methods of HTTP protocol",
       date: "2022-05-30T19:20:14.298Z",
       important: true
+    },
+    {
+      id: 4,
+      content: "THIS is kinda working",
+      date: "2022-05-30T19:20:14.290Z",
+      important: true
     }
   ]
 
